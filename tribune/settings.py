3c5633096e3cabd292d069bfba38abd83bfa109d
Santa claus <santa@northpole.com>
2020-05-20 13:09:50 +0300

    addition of admin page

--- tribune/settings.py
+++ tribune/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tribuness',
         'USER': 'kennedy',
     'PASSWORD':'Kennedy128',
